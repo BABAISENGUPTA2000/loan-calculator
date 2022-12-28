4fa15784da3c7624c0c1a8cb30261add3bb5111b
BABAISENGUPTA2000 <99034786+BABAISENGUPTA2000@users.noreply.github.com>
2022-12-28 12:16:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package MyProgram.start;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc =new Scanner(System.in);
+        int amount= sc.nextInt();
+        float interest = 0.1f;
+        int month= sc.nextInt();
+        for (int i = 0; i < month; i++) {
+            float n= amount*interest;
+            amount= (int) (amount-n);
+            amount++;
+
+        }
+        System.out.println(amount-1);
+    }
+}
